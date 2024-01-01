45fdb09702200cc5ffe16bc31d6b4125bf89d317
percy507 <percymong@163.com>
2024-01-01 16:51:25 +0800

    Add test code

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { window as tauriWindow } from "@tauri-apps/api";
 import { toggleDevtools } from "./commands";
 
 export default function App() {
+  const win = tauriWindow.getCurrent();
+
   return (
     <div>
+      <h3>Current window label: {win.label}</h3>
       <div className="btns">
         <button onClick={toggleDevtools}>open devtools</button>
-        <button
-          onClick={() => {
-            const win = tauriWindow.getCurrent();
-            win.close();
-          }}
-        >
-          close window
-        </button>
+        <button onClick={() => win.close()}>close window</button>
+        <button onClick={() => createWindow()}>create window</button>
       </div>
     </div>
   );
+}
+
+function createWindow() {
+  new tauriWindow.Window("win2", {
+    url: "http://localhost:1420",
+    title: "win-title2",
+    width: 300,
+    height: 300,
+  });
 }
